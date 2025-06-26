eab083ff0bb866f30d23868c776c770aaefbdd84
niladri <niladriforu@gmail.com>
2025-06-25 22:24:32 -0700

    doing my first commit

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 1/task/task.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator.")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What is your pet's name?\n")
+# print("Your name could be", city_name + " " + pet_name)
+print(f"Your name could be {city_name}  {pet_name}")
---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 2/Data Types/task.py
@@ -0,0 +1,18 @@
+#Subscripting
+print("Hello"[0])
+
+#String
+print("123" + "125")
+
+#Integer = Whole Number
+print(123 + 345)
+
+#Large Integers
+print(123_456_789)
+
+#Float = Floating Point Number
+print(3.14159)
+
+#Boolean
+print(True)
+print(False)
